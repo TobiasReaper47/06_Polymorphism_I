fce76378bf3f43e51d7a393f520176ea62b9fdba
TobiasReaper47 <tobiasreaper273@gmail.com>
2025-01-23 11:46:06 +0100

    Polymorphie

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -8,25 +8,57 @@ public class App {
         Worker worker = new Worker();
         Drone drone = new Drone();
 
-        output(queen.doYourJob());
-        output(queen.fly());
+        pollObj(queen);
+        pollObj(worker);
+        pollObj(drone);
+        
+                // output(queen.doYourJob());
+                // output(queen.fly());
+                // output("----------------");
+        
+                // output(worker.doYourJob());
+                // output(worker.fly());
+                // output("----------------");
+        
+                // output(drone.doYourJob());
+                // output(drone.fly());
+                // output("----------------");
+        
+        
+            }              
+        
+    // Polymorphie         
+        
+    private static void pollObj(Honeybee obj){
 
-        output(worker.doYourJob());
-        output(worker.fly());
-
-        output(drone.doYourJob());
-        output(drone.fly());
-
+        output(obj.doYourJob());
+        output(obj.fly());
+        output("----------------");  
 
     }
 
-   
+    // private static void pollObj(Worker obj){
+
+    //     output(obj.doYourJob());
+    //     output(obj.fly());
+    //     output("----------------");  
+
+    // }
+
+    // private static void pollObj(Drone obj){
+
+    //     output(obj.doYourJob());
+    //     output(obj.fly());
+    //     output("----------------");  
+
+    // }
+
    
     private static void output(String outputStr) {
         System.out.println(outputStr);
     }
-
-
-
 }
+
+
+
 
